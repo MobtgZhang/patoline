ce308363c0c8d5522683df7e70ac511644a3869e
MobtgZhang <mobtgzhang@outlook.com>
2026-03-23 02:22:01 +0800

    refactor: drop pa_ocaml preprocess from cesure, patobuild, unicodelib
    - Check in Earley parser syntax expanded with pa_ocaml once.
    - Remove (preprocess (run pa_ocaml ...)) from dune stanzas.

--- cesure/cesure.ml
+++ cesure/cesure.ml
@@ -1,45 +1,91 @@
 open Earley_core
-
-let parser pattern = ''[^- \t\n\r{}]+''
-let parser hyphenation = x:pattern xs:{"-" pattern}* -> x::xs
+let pattern = Earley_core.Earley.declare_grammar "pattern"
+let _ =
+  Earley_core.Earley.set_grammar pattern
+    (Earley_str.regexp ~name:"[^- \\t\\n\\r{}]+" "[^- \t\n\r{}]+"
+       (fun group -> group 0))
+let hyphenation = Earley_core.Earley.declare_grammar "hyphenation"
+let _ =
+  Earley_core.Earley.set_grammar hyphenation
+    (Earley_core.Earley.fsequence pattern
+       (Earley_core.Earley.fsequence
+          (Earley_core.Earley.apply (fun f -> f [])
+             (Earley_core.Earley.fixpoint' (fun l -> l)
+                (Earley_core.Earley.fsequence_ignore
+                   (Earley_core.Earley.string "-" "-")
+                   (Earley_core.Earley.fsequence pattern
+                      (Earley_core.Earley.empty
+                         (fun _default_0 -> _default_0))))
+                (fun x -> fun f -> fun l -> f (List.cons x l))))
+          (Earley_core.Earley.empty (fun xs -> fun x -> x :: xs))))
 let hyphenation = Earley.change_layout hyphenation Earley.no_blank
-
-let parser patt = "\\patterns" "{" pattern* "}"
-let parser hyph = "\\hyphenation" "{" hyphenation* "}"
-
-let parser cesure_file =
-  | ps:patt hy:hyph?[[]] -> (ps, hy)
-  | hy:hyph ps:patt?[[]] -> (ps, hy)
-
+let patt = Earley_core.Earley.declare_grammar "patt"
+let _ =
+  Earley_core.Earley.set_grammar patt
+    (Earley_core.Earley.fsequence_ignore
+       (Earley_core.Earley.string "\\patterns" "\\patterns")
+       (Earley_core.Earley.fsequence_ignore
+          (Earley_core.Earley.string "{" "{")
+          (Earley_core.Earley.fsequence
+             (Earley_core.Earley.apply (fun f -> f [])
+                (Earley_core.Earley.fixpoint' (fun l -> l) pattern
+                   (fun x -> fun f -> fun l -> f (List.cons x l))))
+             (Earley_core.Earley.fsequence_ignore
+                (Earley_core.Earley.string "}" "}")
+                (Earley_core.Earley.empty (fun _default_0 -> _default_0))))))
+let hyph = Earley_core.Earley.declare_grammar "hyph"
+let _ =
+  Earley_core.Earley.set_grammar hyph
+    (Earley_core.Earley.fsequence_ignore
+       (Earley_core.Earley.string "\\hyphenation" "\\hyphenation")
+       (Earley_core.Earley.fsequence_ignore
+          (Earley_core.Earley.string "{" "{")
+          (Earley_core.Earley.fsequence
+             (Earley_core.Earley.apply (fun f -> f [])
+                (Earley_core.Earley.fixpoint' (fun l -> l) hyphenation
+                   (fun x -> fun f -> fun l -> f (List.cons x l))))
+             (Earley_core.Earley.fsequence_ignore
+                (Earley_core.Earley.string "}" "}")
+                (Earley_core.Earley.empty (fun _default_0 -> _default_0))))))
+let cesure_file = Earley_core.Earley.declare_grammar "cesure_file"
+let _ =
+  Earley_core.Earley.set_grammar cesure_file
+    (Earley_core.Earley.alternatives
+       (List.cons
+          (Earley_core.Earley.fsequence hyph
+             (Earley_core.Earley.fsequence
+                (Earley_core.Earley.option [] patt)
+                (Earley_core.Earley.empty (fun ps -> fun hy -> (ps, hy)))))
+          (List.cons
+             (Earley_core.Earley.fsequence patt
+                (Earley_core.Earley.fsequence
+                   (Earley_core.Earley.option [] hyph)
+                   (Earley_core.Earley.empty (fun hy -> fun ps -> (ps, hy)))))
+             [])))
 let blank str pos =
   let rec fn state ((str, pos) as cur) =
-    let (c, str', pos') = Input.read str pos in 
+    let (c, str', pos') = Input.read str pos in
     let next = (str', pos') in
-    match state, c with
-    | `Ini , (' '|'\t'|'\r'|'\n') -> fn `Ini next
-    | `Ini , '%'                  -> fn `Com next
-    | `Com , '\n'                 -> fn `Ini next
-    | `Com , _                    -> fn `Com next
-    | _    , _                    -> cur
-  in fn `Ini (str, pos)
-
+    match (state, c) with
+    | (`Ini, (' '|'\t'|'\r'|'\n')) -> fn `Ini next
+    | (`Ini, '%') -> fn `Com next
+    | (`Com, '\n') -> fn `Ini next
+    | (`Com, _) -> fn `Com next
+    | (_, _) -> cur in
+  fn `Ini (str, pos)
 let parse_file fn =
   let parse = Earley.parse_file cesure_file blank in
   Earley.handle_exception parse fn
-
 let build_cesure_file fn =
   let (pa, hy) = parse_file fn in
   let tree = List.fold_left Hyphen.insert Hyphen.empty pa in
   let tree = List.fold_left Hyphen.insert_exception tree hy in
-  let fn' = (try Filename.chop_extension fn with _ -> fn) ^ ".hdict" in
-  let o = open_out fn' in
-  output_value o tree;
-  close_out o
-
+  let fn' = (try Filename.chop_extension fn with | _ -> fn) ^ ".hdict" in
+  let o = open_out fn' in output_value o tree; close_out o
 let _ =
-  if Array.length Sys.argv > 1 then
-    for i = 1 to Array.length Sys.argv - 1 do
-      build_cesure_file Sys.argv.(i)
+  if (Array.length Sys.argv) > 1
+  then
+    for i = 1 to (Array.length Sys.argv) - 1 do
+      build_cesure_file (Sys.argv.(i))
     done
-  else
-    Printf.eprintf "Usage: %s [File] ... [File]\n%!" Sys.argv.(0)
+  else Printf.eprintf "Usage: %s [File] ... [File]\n%!" (Sys.argv.(0))
--- unicodelib/pa_convert.ml
+++ unicodelib/pa_convert.ml
@@ -1,70 +1,218 @@
 open Earley_core
 open Earley
-open Earley_ocaml
 open Pa_ocaml_prelude
-
-#define LOCATE locate
-
-module Ext = functor(In:Extension) -> struct
-include In
-
-(* FIXME use Blanks.line_comments. *)
-(* Blank function *)
-let blank str pos =
-  let rec fn state ((str, pos) as cur) =
-    let (c, str', pos') = Input.read str pos in
-    let next = (str', pos') in
-    match state, c with
-    | _   , '\255'              -> cur (* EOF reached *)
-    | `Ini, (' ' | '\t' | '\r') -> fn `Ini next
-    | `Ini, '#'                 -> fn `Com next
-    | `Ini, _                   -> cur
-    | `Com, '\n'                -> fn `Ini next
-    | `Com, _                   -> fn `Com next
-  in fn `Ini (str, pos)
-
-(* Parser for hexadecimal integers *)
-let ex_int = parser i:''0x[0-9a-fA-F]+'' -> int_of_string i
-
-(* Single mapping parser *)
-let mapping = change_layout (
-    parser i:ex_int _:''[ \t]*'' j:ex_int?[-1]
-  ) no_blank
-
-let build_file _loc ms =
-  let combine (i, j) e = <:expr<arr.($int:i$) <- $int:j$; $e$>> in
-  let e = List.fold_right combine ms <:expr<arr>> in
-  <:struct<
-    exception Undefined
-
-    let conversion_array : int array =
-      let arr = Array.make 256 (-1) in
-      $e$
-
-    let to_uchar : char -> UChar.uchar = fun c ->
-      let i = Char.code c in
-      if i < 0 || i > 255 then raise Undefined;
-      let u = conversion_array.(i) in
-      if u < 0 then raise Undefined;
-      u
-
-    let to_utf8 : string -> string = fun s ->
-      UTF8.init (String.length s) (fun i -> to_uchar s.[i-1])
-
-    let to_utf16 : string -> string = fun s ->
-      UTF16.init (String.length s) (fun i -> to_uchar s.[i-1])
-
-    let to_utf32 : string -> string = fun s ->
-      UTF32.init (String.length s) (fun i -> to_uchar s.[i-1])
-  >>
-
-let mappings =
-  parser ms:mapping* "\x0A\x1A"? EOF -> build_file _loc ms
-
-let entry_points = (".TXT", Implementation (mappings, blank)) :: entry_points
-
-end
-
-(* Creating and running the extension *)
-module PatolineDefault = Pa_ocaml.Make(Ext(Pa_ocaml_prelude.Initial))
-module M = Pa_main.Start(PatolineDefault)
+open Ast_helper
+module Ext(In:Extension) =
+  struct
+    include In
+    let blank str pos =
+      let rec fn state ((str, pos) as cur) =
+        let (c, str', pos') = Input.read str pos in
+        let next = (str', pos') in
+        match (state, c) with
+        | (_, '\255') -> cur
+        | (`Ini, (' '|'\t'|'\r')) -> fn `Ini next
+        | (`Ini, '#') -> fn `Com next
+        | (`Ini, _) -> cur
+        | (`Com, '\n') -> fn `Ini next
+        | (`Com, _) -> fn `Com next in
+      fn `Ini (str, pos)
+    let ex_int =
+      Earley_core.Earley.fsequence
+        (Earley_str.regexp ~name:"0x[0-9a-fA-F]+" "0x[0-9a-fA-F]+"
+           (fun group -> group 0))
+        (Earley_core.Earley.empty (fun i -> int_of_string i))
+    let mapping =
+      change_layout
+        (Earley_core.Earley.fsequence ex_int
+           (Earley_core.Earley.fsequence_ignore
+              (Earley_str.regexp ~name:"[ \\t]*" "[ \t]*"
+                 (fun group -> group 0))
+              (Earley_core.Earley.fsequence
+                 (Earley_core.Earley.option (-1) ex_int)
+                 (Earley_core.Earley.empty (fun j -> fun i -> (i, j))))))
+        no_blank
+    let build_file _loc ms =
+      let loc = _loc in
+      let combine (i, j) e =
+        Exp.sequence ~loc
+          (Exp.apply ~loc
+             (Exp.ident ~loc { loc; txt = (Ldot ((Lident "Array"), "set")) })
+             [(Nolabel, (Exp.ident ~loc { loc; txt = (Lident "arr") }));
+             (Nolabel, (Exp.constant ~loc (Const.int i)));
+             (Nolabel, (Exp.constant ~loc (Const.int j)))]) e in
+      let arr_fill =
+        List.fold_right combine ms
+          (Exp.ident ~loc { loc; txt = (Lident "arr") }) in
+      let conversion_array_expr =
+        Exp.let_ ~loc Nonrecursive
+          [Vb.mk (Pat.var ~loc { loc; txt = "arr" })
+             (Exp.apply ~loc
+                (Exp.ident ~loc
+                   { loc; txt = (Ldot ((Lident "Array"), "make")) })
+                [(Nolabel, (Exp.constant ~loc (Const.int 256)));
+                (Nolabel, (Exp.constant ~loc (Const.int (-1))))])] arr_fill in
+      let raise_undef =
+        Exp.apply ~loc (Exp.ident ~loc { loc; txt = (Lident "raise") })
+          [(Nolabel,
+             (Exp.construct ~loc { loc; txt = (Lident "Undefined") } None))] in
+      let ty_int = Typ.constr ~loc { loc; txt = (Lident "int") } [] in
+      let ty_int_array =
+        Typ.constr ~loc { loc; txt = (Lident "array") } [ty_int] in
+      let ty_char = Typ.constr ~loc { loc; txt = (Lident "char") } [] in
+      let ty_uchar =
+        Typ.constr ~loc { loc; txt = (Ldot ((Lident "UChar"), "uchar")) } [] in
+      let e_i = Exp.ident ~loc { loc; txt = (Lident "i") } in
+      let e_c = Exp.ident ~loc { loc; txt = (Lident "c") } in
+      let e_s = Exp.ident ~loc { loc; txt = (Lident "s") } in
+      let e_u = Exp.ident ~loc { loc; txt = (Lident "u") } in
+      let e_arr = Exp.ident ~loc { loc; txt = (Lident "conversion_array") } in
+      let cmp_lt a b =
+        Exp.apply ~loc (Exp.ident ~loc { loc; txt = (Lident "<") })
+          [(Nolabel, a); (Nolabel, b)] in
+      let cmp_gt a b =
+        Exp.apply ~loc (Exp.ident ~loc { loc; txt = (Lident ">") })
+          [(Nolabel, a); (Nolabel, b)] in
+      let to_uchar_body =
+        Exp.let_ ~loc Nonrecursive
+          [Vb.mk (Pat.var ~loc { loc; txt = "i" })
+             (Exp.apply ~loc
+                (Exp.ident ~loc
+                   { loc; txt = (Ldot ((Lident "Char"), "code")) })
+                [(Nolabel, e_c)])]
+          (Exp.ifthenelse ~loc (cmp_lt e_i (Exp.constant ~loc (Const.int 0)))
+             raise_undef
+             (Some
+                (Exp.ifthenelse ~loc
+                   (cmp_gt e_i (Exp.constant ~loc (Const.int 255)))
+                   raise_undef
+                   (Some
+                      (Exp.let_ ~loc Nonrecursive
+                         [Vb.mk (Pat.var ~loc { loc; txt = "u" })
+                            (Exp.apply ~loc
+                               (Exp.ident ~loc
+                                  {
+                                    loc;
+                                    txt = (Ldot ((Lident "Array"), "get"))
+                                  }) [(Nolabel, e_arr); (Nolabel, e_i)])]
+                         (Exp.ifthenelse ~loc
+                            (cmp_lt e_u (Exp.constant ~loc (Const.int 0)))
+                            raise_undef (Some e_u))))))) in
+      let utf8_body =
+        Exp.apply ~loc
+          (Exp.ident ~loc { loc; txt = (Ldot ((Lident "UTF8"), "init")) })
+          [(Nolabel,
+             (Exp.apply ~loc
+                (Exp.ident ~loc
+                   { loc; txt = (Ldot ((Lident "String"), "length")) })
+                [(Nolabel, e_s)]));
+          (Nolabel,
+            (Exp.fun_ ~loc Nolabel None (Pat.var ~loc { loc; txt = "i" })
+               (Exp.apply ~loc
+                  (Exp.ident ~loc { loc; txt = (Lident "to_uchar") })
+                  [(Nolabel,
+                     (Exp.apply ~loc
+                        (Exp.ident ~loc
+                           { loc; txt = (Ldot ((Lident "String"), "get")) })
+                        [(Nolabel, e_s);
+                        (Nolabel,
+                          (Exp.apply ~loc
+                             (Exp.ident ~loc { loc; txt = (Lident "-") })
+                             [(Nolabel,
+                                (Exp.ident ~loc { loc; txt = (Lident "i") }));
+                             (Nolabel, (Exp.constant ~loc (Const.int 1)))]))]))])))] in
+      let utf16_body =
+        Exp.apply ~loc
+          (Exp.ident ~loc { loc; txt = (Ldot ((Lident "UTF16"), "init")) })
+          [(Nolabel,
+             (Exp.apply ~loc
+                (Exp.ident ~loc
+                   { loc; txt = (Ldot ((Lident "String"), "length")) })
+                [(Nolabel, e_s)]));
+          (Nolabel,
+            (Exp.fun_ ~loc Nolabel None (Pat.var ~loc { loc; txt = "i" })
+               (Exp.apply ~loc
+                  (Exp.ident ~loc { loc; txt = (Lident "to_uchar") })
+                  [(Nolabel,
+                     (Exp.apply ~loc
+                        (Exp.ident ~loc
+                           { loc; txt = (Ldot ((Lident "String"), "get")) })
+                        [(Nolabel, e_s);
+                        (Nolabel,
+                          (Exp.apply ~loc
+                             (Exp.ident ~loc { loc; txt = (Lident "-") })
+                             [(Nolabel,
+                                (Exp.ident ~loc { loc; txt = (Lident "i") }));
+                             (Nolabel, (Exp.constant ~loc (Const.int 1)))]))]))])))] in
+      let utf32_body =
+        Exp.apply ~loc
+          (Exp.ident ~loc { loc; txt = (Ldot ((Lident "UTF32"), "init")) })
+          [(Nolabel,
+             (Exp.apply ~loc
+                (Exp.ident ~loc
+                   { loc; txt = (Ldot ((Lident "String"), "length")) })
+                [(Nolabel, e_s)]));
+          (Nolabel,
+            (Exp.fun_ ~loc Nolabel None (Pat.var ~loc { loc; txt = "i" })
+               (Exp.apply ~loc
+                  (Exp.ident ~loc { loc; txt = (Lident "to_uchar") })
+                  [(Nolabel,
+                     (Exp.apply ~loc
+                        (Exp.ident ~loc
+                           { loc; txt = (Ldot ((Lident "String"), "get")) })
+                        [(Nolabel, e_s);
+                        (Nolabel,
+                          (Exp.apply ~loc
+                             (Exp.ident ~loc { loc; txt = (Lident "-") })
+                             [(Nolabel,
+                                (Exp.ident ~loc { loc; txt = (Lident "i") }));
+                             (Nolabel, (Exp.constant ~loc (Const.int 1)))]))]))])))] in
+      [Str.exception_ ~loc
+         (Te.mk_exception ~loc (Te.decl ~loc { loc; txt = "Undefined" }));
+      Str.value ~loc Nonrecursive
+        [Vb.mk
+           (Pat.constraint_ ~loc
+              (Pat.var ~loc { loc; txt = "conversion_array" }) ty_int_array)
+           conversion_array_expr];
+      Str.value ~loc Nonrecursive
+        [Vb.mk
+           (Pat.constraint_ ~loc (Pat.var ~loc { loc; txt = "to_uchar" })
+              (Typ.arrow ~loc Nolabel ty_char ty_uchar))
+           (Exp.fun_ ~loc Nolabel None (Pat.var ~loc { loc; txt = "c" })
+              to_uchar_body)];
+      Str.value ~loc Nonrecursive
+        [Vb.mk (Pat.var ~loc { loc; txt = "to_utf8" })
+           (Exp.fun_ ~loc Nolabel None (Pat.var ~loc { loc; txt = "s" })
+              utf8_body)];
+      Str.value ~loc Nonrecursive
+        [Vb.mk (Pat.var ~loc { loc; txt = "to_utf16" })
+           (Exp.fun_ ~loc Nolabel None (Pat.var ~loc { loc; txt = "s" })
+              utf16_body)];
+      Str.value ~loc Nonrecursive
+        [Vb.mk (Pat.var ~loc { loc; txt = "to_utf32" })
+           (Exp.fun_ ~loc Nolabel None (Pat.var ~loc { loc; txt = "s" })
+              utf32_body)]]
+    let mappings =
+      Earley_core.Earley.fsequence
+        (Earley_core.Earley.apply (fun f -> f [])
+           (Earley_core.Earley.fixpoint' (fun l -> l) mapping
+              (fun x -> fun f -> fun l -> f (List.cons x l))))
+        (Earley_core.Earley.fsequence
+           (Earley_core.Earley.option None
+              (Earley_core.Earley.apply (fun x -> Some x)
+                 (Earley_core.Earley.string "\n\026" "\n\026")))
+           (Earley_core.Earley.fsequence_ignore (Earley_core.Earley.eof ())
+              (Earley_core.Earley.empty_pos
+                 (fun __loc__start__buf ->
+                    fun __loc__start__pos ->
+                      fun __loc__end__buf ->
+                        fun __loc__end__pos ->
+                          let _loc =
+                            locate __loc__start__buf __loc__start__pos
+                              __loc__end__buf __loc__end__pos in
+                          fun _default_0 -> fun ms -> build_file _loc ms))))
+    let entry_points = (".TXT", (Implementation (mappings, blank))) ::
+      entry_points
+  end
+module PatolineDefault = (Pa_ocaml.Make)((Ext)(Pa_ocaml_prelude.Initial))
+module M = (Pa_main.Start)(PatolineDefault)
